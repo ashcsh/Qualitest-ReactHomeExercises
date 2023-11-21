191287f77ecd5444fff998906ffdbdbd9d28b02f
Victor <91851706+ashcsh@users.noreply.github.com>
2023-11-22 01:27:58 +0200

    notFoundPage

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,21 @@
 import { FC } from "react";
 import { PageWrapper } from "./styles";
+import { Button } from "../components/Button/Button";
+import { useNavigate } from "react-router-dom"
 
-export const PageNotFound: FC = () => (
-  <PageWrapper>
-    <h1>Page not found</h1>
-  </PageWrapper>
-);
+export const PageNotFound: FC = () => {
+  const navigate = useNavigate()
+  const handleNavigateHome = () => {
+    navigate("/")
+  }
+
+  return (
+    <PageWrapper>
+      <h1>Page not found</h1>
+      <div>
+        <Button ariaLabel="Home Button" variant="primary" onClick={handleNavigateHome}>Go Back Home</Button>
+      </div>
+    </PageWrapper>
+  )
+}
+
